3b32bf078d4d2c6d667e43e59b3b29f6dfbd55b4
Wallace Coimbra <wallace.coimbra@bentley.com>
2023-10-31 10:23:42 -0300

    finished close product, now is 'Carrinho' lesson

--- 4.Techno Projeto/techno/app.js
+++ 4.Techno Projeto/techno/app.js
@@ -1,13 +1,27 @@
 const vm = new Vue({
     el: "#app",
     data: {
-        produtos: []
+        produtos: [],
+        produto: ""
+    },
+    filters: {
+        numeroPreco(valor) {
+            return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
+        }
     },
     methods: {
         fetchProdutos() {
             fetch("./api/produtos.json")
                 .then(r => r.json())
                 .then(r => this.produtos = r)
+        },
+        fetchProduto(id) {
+            fetch(`./api/produtos/${id}/dados.json`)
+                .then(r => r.json())
+                .then(r => this.produto = r)
+        },
+        fecharModal({ target, currentTarget }) {
+            if (target === currentTarget) this.produto = false
         }
     },
     created() {
